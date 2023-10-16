1a5cbdee908067cafba84c43e90443e2a0e42dfd
charles-mitch200 <charlesreign2@gmail.com>
2023-10-16 21:48:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Book Class: Represents a Book
+class Book {
+    constructor (title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI tasks
+class UI {
+    static displayBooks() {
+        const storedBooks = [
+            
+        ];
+    }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book (from UI and local storage)
